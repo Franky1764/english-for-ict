9db1e4d3f1a2837e5d72ad8bf400112e6f0cdaf1
franky1764 <franky1764@hotmail.com>
2025-04-26 19:14:14 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'english-for-ict',
+  webDir: 'www'
+};
+
+export default config;
