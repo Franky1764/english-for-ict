700f5158e77689d45d3e293d99cfbf00d8523adc
Daniela <franky1764@hotmail.com>
2025-04-26 20:23:50 -0400

    Sync local project with remote repository

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,36 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'welcome',
+    loadComponent: () => import('./pages/welcome/welcome.page').then( m => m.WelcomePage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'vocabulary',
+    loadComponent: () => import('./pages/vocabulary/vocabulary.page').then( m => m.VocabularyPage)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.page').then( m => m.DashboardPage)
+  },
+  {
+    path: 'challenges',
+    loadComponent: () => import('./pages/challenges/challenges.page').then( m => m.ChallengesPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
+  },
+  {
+    path: 'community',
+    loadComponent: () => import('./pages/community/community.page').then( m => m.CommunityPage)
+  },
 ];
