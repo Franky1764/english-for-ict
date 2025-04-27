07d9dc1881359a55b65f9ce8782aed7b3559ac09
Daniela <franky1764@hotmail.com>
2025-04-27 02:16:56 -0400

    Complete Login Page design and navigation flow

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,36 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonContent, IonButton, IonInput, IonItem, IonLabel, IonIcon, IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonInput, IonLabel, IonItem } from '@ionic/angular/standalone';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonTitle, IonToolbar, IonHeader, 
-    IonContent,
-    IonButton,
-    IonInput,
-    IonItem,
-    IonLabel,
-    IonIcon,
-    CommonModule,
-    FormsModule
-  ],
+  imports: [IonContent, IonButton, IonInput, IonLabel, IonItem, CommonModule, FormsModule],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
-
-  ngOnInit() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   login() {
-    console.log('Login con:', this.email, this.password);
-    // Aquí luego conectamos a Firebase
+    this.authService.login(this.email, this.password);
+  }
+
+  handleRegister() {
+    this.router.navigate(['/register']);
   }
 }
-
