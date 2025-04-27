4f9bf5544efea5c1fdb95477ec99e50bbc78bea0
Daniela <franky1764@hotmail.com>
2025-04-26 21:58:54 -0400

    Complete and fix Register Page with standalone components

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,20 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonInput, IonLabel, IonItem, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonInput, IonLabel,IonItem,IonIcon, CommonModule, FormsModule]
 })
 export class RegisterPage implements OnInit {
+    email: string = '';
+    password: string = '';
+    confirmPassword: string = '';
+    name: string = '';
+    phone: string = '';
+    comuna: string = '';
+    region: string = '';
+    showPassword: boolean = false;
+    showConfirmPassword: boolean = false;
+    showPasswordIcon: string = 'eye-off-outline';
+    showConfirmPasswordIcon: string = 'eye-off-outline';
 
-  constructor() { }
+    constructor(private router: Router) {}
 
-  ngOnInit() {
-  }
 
+    ngOnInit() {}
+
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+      this.showPasswordIcon = this.showPassword ? 'eye-outline' : 'eye-off-outline';
+    }
+
+    toggleConfirmPasswordVisibility() {
+      this.showConfirmPassword = !this.showConfirmPassword;
+      this.showConfirmPasswordIcon = this.showConfirmPassword ? 'eye-outline' : 'eye-off-outline';
+    }
+
+    register() {
+      console.log('Registro exitoso:', {
+        email: this.email,
+        password: this.password,
+        name: this.name,
+        phone: this.phone,
+        comuna: this.comuna,
+        region: this.region
+      });
+      // Redirigir a la página de inicio de sesión después del registro exitoso
+       this.router.navigate(['/login']);
+    }
 }
